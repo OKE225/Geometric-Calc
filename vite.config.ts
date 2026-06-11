17cfc3eb3e6080268f0f106a7168e65a0e1f162e
OKE225 <patryk.wroblewski.2006@gmail.com>
2026-06-11 12:08:52 +0200

    Add base to url

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // base: "/Geometric-Calc/", // add on deploy
+  base: "/Geometric-Calc/",
 });
