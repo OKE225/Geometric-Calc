c615471f8f92cbfa95ada8ac4ee8e2243b2e12f4
OKE225 <patryk.wroblewski.2006@gmail.com>
2026-06-11 14:19:46 +0200

    Delete maxLength

--- src/components/Geometry3D.tsx
+++ src/components/Geometry3D.tsx
@@ -3,35 +3,28 @@ import { Canvas } from "@react-three/fiber";
 import type { ReactNode } from "react";
 
 interface Props {
-  maxLength?: number;
   children: ReactNode;
   offSecondLight?: boolean;
 }
 
-const Geometry3D = ({ maxLength = 1, children, offSecondLight }: Props) => {
+const Geometry3D = ({ children, offSecondLight }: Props) => {
   return (
     <Canvas
       camera={{
-        position: [5 * maxLength, 5 * maxLength, 15 * maxLength],
+        position: [5, 5, 15],
         fov: 20,
         far: 10_000,
       }}>
       <ambientLight intensity={2.25} />
-      <directionalLight
-        intensity={1.25}
-        position={[2 * maxLength, 3 * maxLength, 1 * maxLength]}
-      />
+      <directionalLight intensity={1.25} position={[2, 3, 1]} />
 
       {!offSecondLight && (
-        <directionalLight
-          intensity={1.25}
-          position={[-2 * maxLength, -3 * maxLength, -1 * maxLength]}
-        />
+        <directionalLight intensity={1.25} position={[-2, -3, -1]} />
       )}
 
       <OrbitControls autoRotate autoRotateSpeed={-4} enablePan={false} />
 
-      <mesh position={[0, 0, 0]} scale={1.5}>
+      <mesh position={[0, 0, 0]} scale={1}>
         {children}
       </mesh>
     </Canvas>
